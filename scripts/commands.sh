550d13e95fc54db3653fb94bfb345e8c259c0f3a
criselecam <criselecam@gmail.com>
2024-12-07 16:14:31 -0300

    Adicionei o docker-compose-yml

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -3,7 +3,7 @@
 # O shell irá encerrar a execução do script quando um comando falhar
 set -e
 
-while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+while ! nc -z "$POSTGRES_HOST" "$POSTGRES_PORT"; do
   echo "🟡 Waiting for Postgres Database Startup ($POSTGRES_HOST $POSTGRES_PORT) ..."
   sleep 2
 done
